3f6e387fd0b56b57770f7e34809a33e00c383a94
Left Burner <lefttheburneron@gmail.com>
2020-02-24 02:22:23 -0500

    Add normalize data script which is required for data merge.

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ This data on the master branch is used by live apps, so if you want to suggest a
 ## contributing
 
 Pull requests to add or update data in this repository are welcome! If you're worried about future or current employers seeing your GitHub contribution history, consider creating a separate account to ensure your contributions are anonymous.
+
+## normalizing data before merging
+
+Data must be normalized to a specific structure and order before being merged. This makes it easier to review the data and compare it against other alphabetized lists. You can do this by running the following scripts:
+
+* `scripts/check-if-normalized` to see whether the data is normalized (requires the `diff` shell command, which is available on most Unix-based systems, to be available)
+* `scripts/normalize-data` which actually updates the data to be normalized
---
+++ scripts/lib/normalizeData.js
@@ -0,0 +1,140 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const DATA_DIR = path.join(__dirname, '..', '..');
+
+const transforms = {
+  'members.json'(data) {
+    const warnings = [];
+    const transformedData = data
+      .sort(function compare(a, b) {
+        let locationA = a.location || '';
+        let locationB = b.location || '';
+
+        // ignore "GWU " prefix when sorting
+        if (locationA.startsWith('GWU ')) {
+          locationA = locationA.slice(4);
+        }
+        if (locationB.startsWith('GWU ')) {
+          locationB = locationB.slice(4);
+        }
+
+        if (locationA === locationB) {
+          return 0;
+        }
+        return locationA > locationB ? 1 : -1;
+      })
+      .map(function(member) {
+        const {
+          location,
+          lat,
+          lng,
+          country,
+          isChapter,
+          chapterInfo: {
+            description,
+            applicationLink,
+            twitter,
+            email,
+            website,
+            ...otherChapterInfo
+          } = {},
+          ...other
+        } = member;
+
+        // errors
+        if (!location) {
+          throw new Error(
+            'ERROR: Missing location in object: ' + JSON.stringify(member, null, 1)
+          );
+        }
+        for (const { name, isInvalid } of [
+          { name: 'lat', isInvalid: val => typeof val !== 'number' },
+          { name: 'lng', isInvalid: val => typeof val !== 'number' },
+          { name: 'country', isInvalid: val => !val },
+          { name: 'isChapter', isInvalid: val => typeof val !== 'boolean' },
+          { name: 'chapterInfo', isInvalid: val => isChapter && typeof val !== 'object' }
+        ]) {
+          if (isInvalid(member[name])) {
+            throw new Error(
+              `ERROR: Invalid property "${name}" for location "${location}": ${JSON.stringify(member[name], null, 1)}`
+            );
+          }
+        }
+
+        // warnings
+        for (const infoPropertyName of [
+          'description',
+          'applicationLink',
+          'twitter',
+          'email',
+          'website'
+        ]) {
+          if (isChapter) {
+            if (!member.chapterInfo[infoPropertyName]) {
+              warnings.push(
+                `WARNING: missing chapter info "${infoPropertyName}" for location "${location}"`
+              );
+            }
+          } else {
+            if ((member.chapterInfo || {})[infoPropertyName]) {
+              warnings.push(
+                `WARNING: unexpected chapter info "${infoPropertyName}" for location "${location}" which is not marked as a chapter`
+              );
+            }
+          }
+        }
+        if (Object.keys(other).length) {
+          warnings.push(
+            `WARNING: Unrecognized properties for location "${location}": ${JSON.stringify(other, null, 1)}`
+          );
+        }
+        if (Object.keys(otherChapterInfo).length) {
+          warnings.push(
+            `WARNING: Unrecognized chapter info for location "${location}": ${JSON.stringify(otherChapterInfo, null, 1)}`
+          );
+        }
+
+        return {
+          location,
+          lat,
+          lng,
+          country,
+          isChapter,
+          chapterInfo: {
+            description,
+            applicationLink,
+            twitter,
+            email,
+            website,
+            ...otherChapterInfo
+          },
+          ...other
+        };
+      });
+
+    return {
+      data: transformedData,
+      warnings
+    };
+  }
+};
+
+module.exports = {
+  normalizeData() {
+    const normalizedData = {};
+    for (const [filepath, normalize] of Object.entries(transforms)) {
+      const pathname = path.join(DATA_DIR, filepath);
+      const data = require(pathname);
+      const { data: normalized, warnings } = normalize(data);
+      normalizedData[pathname] = {
+        content: JSON.stringify(normalized, null, 2) + '\n',
+        warnings
+      };
+    }
+    return normalizedData;
+  },
+  transforms
+};
